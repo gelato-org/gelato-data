ecdb45870434a192b0bd1582eaa4f61f250daf44
Robert Forkel <xrotwang@googlemail.com>
2022-06-29 20:35:45 +0200

    scaffolding

--- setup.py
+++ setup.py
@@ -1,41 +1,24 @@
-from setuptools import setup, find_packages
+from setuptools import setup
 
-
-requires = [
-    # list required third-party packages here
-    'appdirs',
-    'requests',
-    'clldutils>=1.5.4',
-    'pyglottolog',
-    'tabulate',
-    'tqdm',
-    'bagit>=1.5.4',
-    'pybtex',
-    'openpyxl',
-]
 
 setup(
-    name='pygelato',
-    version='0.0',
-    description='python package for the GELATO repository',
-    long_description='',
-    classifiers=[
-        "Programming Language :: Python",
-        "Programming Language :: Python :: 2",
-        "Programming Language :: Python :: 2.7",
-        "Programming Language :: Python :: 3",
-        "Programming Language :: Python :: 3.4",
-    ],
-    author='Robert Forkel',
-    author_email='forkel@shh.mpg.de',
-    url='',
-    keywords='data linguistics genetics',
-    packages=find_packages(),
+    name='cldfbench_gelato',
+    py_modules=['cldfbench_gelato'],
     include_package_data=True,
     zip_safe=False,
-    install_requires=requires,
     entry_points={
-        'console_scripts': ['gelato=pygelato.__main__:main'],
+        'cldfbench.dataset': [
+            'gelato=cldfbench_gelato:Dataset',
+        ]
     },
-    tests_require=[],
-    test_suite="pygelato")
+    install_requires=[
+        'cldfbench',
+        'csvw',
+        'pycldf',
+    ],
+    extras_require={
+        'test': [
+            'pytest-cldf',
+        ],
+    },
+)
